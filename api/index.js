1f2fb1c2c62cf812b7925ba167f5c126f6c30cda
Nicolas Hovart <hovart.nicolas@gmail.com>
2025-02-13 17:46:51 +0100

    Add catch all route

--- api/index.js
+++ api/index.js
@@ -86,16 +86,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/lobbies', lobbyRoutes);
 app.use('/api/messages', messageRoutes);
 
-// app.all('*', (req, res) => {
-//     res.status(404);
-//     if (req.accepts('html')) {
-//         res.sendFile(path.join(__dirname, 'views', '404.html'));
-//     } else if (req.accepts('json')) {
-//         res.json({ "error": "404 Not Found" });
-//     } else {
-//         res.type('txt').send("404 Not Found");
-//     }
-// });
+// Catch-all route
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
 if (process.env.NODE_ENV == 'development')
     app.listen(port, () => {
